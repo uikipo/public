9a32186deadcd9c631716673150f9eb317f8f23c
Kanade <kanade@nightcord.com.de>
2025-02-25 08:42:07 -0500

    meta

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://kdx.nightcord.com.de',
 	integrations: [
 		starlight({
 			title: 'KanadeDX',
